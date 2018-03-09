9ad950f6992b4f378e97ece522fff516bbacf25c
Akhilesh <akhilesh.deowanshi@hotmail.com>
2018-03-09 11:23:26 -0800

    fixes

--- src/main/java/com/sample/polling/service/LongPolling.java
+++ src/main/java/com/sample/polling/service/LongPolling.java
@@ -20,7 +20,7 @@ public class LongPolling
 
         @Async
     public String waitFor5To15Seconds() {
-        int  first = rand.nextInt(2) + 1;
+        int  first = rand.nextInt(5) + 1;
          long start = System.currentTimeMillis();
          try {
              long sleepTimeInMilli = 5000L + 1000L*secureRandom.nextInt(10);  // 5 - 15 seconds
@@ -29,10 +29,10 @@ public class LongPolling
              ie.printStackTrace();
          }
 
-        int  last = rand.nextInt(9) + 1;
+        int  last = rand.nextInt(5) + 1;
          long end = System.currentTimeMillis();
         if(first != last)
-            return "After " + (end-start) +"ms the server responds with." + "Old Data" + first + "New Data " + last;
+            return "After " + (end-start) +"ms the server responds with." + "Old Data " + first + "New Data " + last;
         else
             return waitFor5To15Seconds();
     }
