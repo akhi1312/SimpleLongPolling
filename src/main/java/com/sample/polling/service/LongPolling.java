016ac3f3c23424ab18e253560bfdafa745843521
akhi1312 <akhilesh.deowanshi@sjsu.edu>
2018-03-12 12:51:59 -0700

    Update LongPolling.java

--- src/main/java/com/sample/polling/service/LongPolling.java
+++ src/main/java/com/sample/polling/service/LongPolling.java
@@ -11,31 +11,31 @@ public class LongPolling
 
 
     {
-//         private static SecureRandom secureRandom = new SecureRandom();
-//         static {
-//         secureRandom.setSeed( System.currentTimeMillis() );
-//     }
+        private static SecureRandom secureRandom = new SecureRandom();
+        static {
+        secureRandom.setSeed( System.currentTimeMillis() );
+    }
 
-//         static Random rand = new Random();
+        static Random rand = new Random();
 
-//     @Async
-//     public String waitFor5To15Seconds() {
-//         int  first = rand.nextInt(5) + 1;
-//          long start = System.currentTimeMillis();
-//          try {
-//              long sleepTimeInMilli = 1000L*secureRandom.nextInt(10);  // 5 - 15 seconds
-//              Thread.sleep(sleepTimeInMilli);
-//          } catch (InterruptedException ie) {
-//              ie.printStackTrace();
-//          }
+    @Async
+    public String waitFor5To15Seconds() {
+        int  first = rand.nextInt(5) + 1;
+         long start = System.currentTimeMillis();
+         try {
+             long sleepTimeInMilli = 1000L*secureRandom.nextInt(10);  // 5 - 15 seconds
+             Thread.sleep(sleepTimeInMilli);
+         } catch (InterruptedException ie) {
+             ie.printStackTrace();
+         }
 
-//         int  last = rand.nextInt(5) + 1;
-//          long end = System.currentTimeMillis();
-//         if(first != last)
-//             return "After " + (end-start) +" ms the server responds with Echo";
-//         else
-//             return waitFor5To15Seconds();
-//     }
+        int  last = rand.nextInt(5) + 1;
+         long end = System.currentTimeMillis();
+        if(first != last)
+            return "After " + (end-start) +" ms the server responds with Echo";
+        else
+            return waitFor5To15Seconds();
+    }
 
 }
 
