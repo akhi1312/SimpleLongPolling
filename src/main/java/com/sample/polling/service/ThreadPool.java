f8c348ee31c86a2f0028e57ce5cd991371b88310
Akhilesh <akhilesh.deowanshi@hotmail.com>
2018-03-15 00:02:40 -0700

    removed extra code

--- src/main/java/com/sample/polling/service/ThreadPool.java
+++ src/main/java/com/sample/polling/service/ThreadPool.java
@@ -3,19 +3,21 @@ package com.sample.polling.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
+@PropertySource("classpath:/Values.properties")
 public class ThreadPool{
 
 
-        @Value("${contacts.thread.core-pool}")
+        @Value("${spring.thread.core-pool}")
         private int corePoolSize;
 
-        @Value("${contacts.thread.max-pool}")
+        @Value("${spring.thread.max-pool}")
         private int maxPoolSize;
 
-        @Value("${contacts.queue.capacity}")
+        @Value("${spring.queue.capacity}")
         private int queueCapacity;
 
 
